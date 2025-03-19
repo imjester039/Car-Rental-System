6102852dc0c74cd443465a15f1c137ef812292a6
Jester <135610341+imjester039@users.noreply.github.com>
2025-03-19 10:03:55 +0700

    Review.java

--- CarRentalSystem/src/carRentalSystem/Review.java
+++ CarRentalSystem/src/carRentalSystem/Review.java
@@ -8,10 +8,10 @@ public class Review implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private Customer customer;
 	private Car car;
-	private int rating; // 1-5 stars
+	private int rating;
 	private String comment;
 	private LocalDateTime reviewDate;
-	private LocalDateTime returnDate; // Thêm trường returnDate
+	private LocalDateTime returnDate; 
 
 	public Review(Customer customer, Car car, int rating, String comment, LocalDateTime reviewDate,
 			LocalDateTime returnDate) {
@@ -25,16 +25,15 @@ public class Review implements Serializable {
 
 	@Override
 	public String toString() {
+		
 		// Kiểm tra nếu returnDate là null
 		String formattedReturnDate = (returnDate != null)
 				? returnDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"))
 				: "N/A"; // Giá trị mặc định nếu returnDate là null
-
-		// Định dạng thông tin review
+		
 		String reviewInfo = String.format("Customer: %s | Rating: %d/5 | Comment: %s", customer.getName(), rating,
 				comment);
 
-		// Kết hợp ngày trả xe và thông tin review
 		return formattedReturnDate + "\n" + reviewInfo;
 	}
 }
